9713e478d2b707796fa1930dcadd9af1622e8906
Marco Cilloni <marco.cilloni@zuru.com>
2024-01-26 08:38:57 +0100

    errors: decouple from errno

--- include/dicey/errors.h
+++ include/dicey/errors.h
@@ -1,26 +1,21 @@
 #if !defined(HHQPUVHYDW_ERRORS_H)
 #define HHQPUVHYDW_ERRORS_H
 
-#include <errno.h>
-
 #include "dicey_export.h"
 
 #if defined (__cplusplus)
 extern "C" {
 #endif
 
-// this enum uses same values as errno.h for comparable value. While the errno values are not defined by the C standard,
-// they are defined by POSIX, Win32 and C++, and widely used. We use them here to make it easier to integrate with
-// existing code.
 enum dicey_error {
     DICEY_OK = 0,
     
-    DICEY_EAGAIN    = -EAGAIN,
-    DICEY_ENOMEM    = -ENOMEM,
-    DICEY_EINVAL    = -EINVAL,
-    DICEY_ENODATA   = -ENODATA,
-    DICEY_EBADMSG   = -EBADMSG,
-    DICEY_EOVERFLOW = -EOVERFLOW,
+    DICEY_EAGAIN    = -0x000B,
+    DICEY_ENOMEM    = -0x000C,
+    DICEY_EINVAL    = -0x0016,
+    DICEY_ENODATA   = -0x003D,
+    DICEY_EBADMSG   = -0x004A,
+    DICEY_EOVERFLOW = -0x004B,
 
     DICEY_EPATH_TOO_LONG  = -0x1000,
     DICEY_ETUPLE_TOO_LONG = -0x1001,
