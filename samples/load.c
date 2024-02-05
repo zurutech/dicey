7b4bf959e455256d683ed9fa5e08e29249294892
Marco Cilloni <marco.cilloni@zuru.com>
2024-02-05 09:39:11 +0100

    samples/load: kindly tell Microsoft to shut up

--- samples/load.c
+++ samples/load.c
@@ -1,5 +1,8 @@
 // Copyright (c) 2014-2024 Zuru Tech HK Limited, All rights reserved.
 
+// thank you MS, but just no
+#define _CRT_SECURE_NO_WARNINGS 1
+
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
