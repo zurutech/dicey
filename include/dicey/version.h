0884894840a6fb586199939406e1899836af7ac0
Marco Cilloni <marco.cilloni@zuru.com>
2024-02-08 16:24:22 +0100

    version: correctly reflect that library and protocols have different versions

--- include/dicey/version.h
+++ include/dicey/version.h
@@ -9,9 +9,16 @@
 extern "C" {
 #endif
 
-#define DICEY_VERSION_MAJOR 0
-#define DICEY_VERSION_REVISION 1
-#define DICEY_VERSION_STRING #DICEY_VERSION_MAJOR "r" #DICEY_VERSION_REVISION
+/**
+ * @brief Major version number of the Dicey protocol implemented by this library
+ * @note  The Dicey protocol is identified by a major version number and a revision number.
+ *        The major version number is incremented every time the protocol is changed.
+ *        The revision number is incremented every time the protocol is patched without breaking compatibility or
+ *        adding new features.
+ */
+#define DICEY_PROTO_MAJOR 0
+#define DICEY_PROTO_REVISION 1
+#define DICEY_PROTO_STRING #DICEY_PROTO_MAJOR "r" #DICEY_PROTO_REVISION
 
 /**
  * @brief Structure representing the version information in a "hello" packet.
@@ -21,8 +28,11 @@ struct dicey_version {
     uint16_t revision; /**< Revision number */
 };
 
-#define DICEY_VERSION_CURRENT                                                                                          \
-    ((struct dicey_version) { .major = DICEY_VERSION_MAJOR, .revision = DICEY_VERSION_REVISION })
+#define DICEY_VERSION_CURRENT ((struct dicey_version) { .major = DICEY_PROTO_MAJOR, .revision = DICEY_PROTO_REVISION })
+
+#define DICEY_LIB_VERSION_MAJOR 0
+#define DICEY_LIB_VERSION_MINOR 0
+#define DICEY_LIB_VERSION_REVISION 1
 
 #if defined(__cplusplus)
 }
