996344f3c4ebba8271e132e0d44f56fda655e38d
Marco Cilloni <marco.cilloni@zuru.com>
2024-03-06 15:37:42 +0100

    address: shut CRT up

--- src/ipc/address.c
+++ src/ipc/address.c
@@ -1,6 +1,6 @@
 // Copyright (c) 2014-2024 Zuru Tech HK Limited, All rights reserved.
 
-#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_NONSTDC_NO_DEPRECATE 1
 #include <assert.h>
 #include <stddef.h>
 #include <stdlib.h>
