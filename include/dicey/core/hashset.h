a7c08806bfcd6989ab40a46cc9fc1dc09a0f8627
Marco Cilloni <marco.cilloni@zuru.com>
2024-03-08 16:05:16 +0100

    hashset: added hashset

---
+++ include/dicey/core/hashset.h
@@ -0,0 +1,28 @@
+// Copyright (c) 2014-2024 Zuru Tech HK Limited, All rights reserved.
+
+#if !defined(KVOFDWUXXQ_HASHSET_H)
+#define KVOFDWUXXQ_HASHSET_H
+
+#include "hashtable.h"
+
+struct dicey_hashset;
+
+struct dicey_hashset_iter {
+    struct dicey_hashtable_iter _inner;
+};
+
+DICEY_EXPORT struct dicey_hashset *dicey_hashset_new(void);
+
+DICEY_EXPORT void dicey_hashset_delete(struct dicey_hashset *table);
+
+DICEY_EXPORT struct dicey_hashset_iter dicey_hashset_iter_start(struct dicey_hashset *table);
+DICEY_EXPORT bool dicey_hashset_iter_next(struct dicey_hashset_iter *iter, const char **key);
+
+DICEY_EXPORT bool dicey_hashset_contains(struct dicey_hashset *table, const char *key);
+DICEY_EXPORT bool dicey_hashset_remove(struct dicey_hashset *table, const char *key);
+
+DICEY_EXPORT enum dicey_hash_set_result dicey_hashset_set(struct dicey_hashset **set, const char *key);
+
+DICEY_EXPORT uint32_t dicey_hashset_size(struct dicey_hashset *table);
+
+#endif // KVOFDWUXXQ_HASHSET_H
---
+++ src/sup/hashset.c
@@ -0,0 +1,68 @@
+// Copyright (c) 2014-2024 Zuru Tech HK Limited, All rights reserved.
+
+#include <assert.h>
+#include <stdalign.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include <dicey/core/hashset.h>
+#include <dicey/core/hashtable.h>
+
+// note: the C standard says that "A pointer to an object or incomplete type may be converted to a pointer to a
+// different object or incomplete type. If the resulting pointer is not correctly aligned for the pointed-to type, the
+// behavior is undefined", so this whole file may potentially be full of UB. Still, we only support a handful of CPU
+// architectures and OSes and not even Minesweeper would work if you couldn't just cast stuff around willy nilly, so
+// I'll live with the consequences of my actions for the sake of a nice API, thank you very much.
+
+static_assert(
+    alignof(struct dicey_hashset *) == alignof(struct dicey_hashtable *),
+    "Well, you sure bought a weird CPU sir"
+);
+
+// a non-null marker, helpful to distinguish non-set (NULL) from set (phony)
+static int phony = 0;
+
+struct dicey_hashset *dicey_hashset_new(void) {
+    return (struct dicey_hashset *) dicey_hashtable_new();
+}
+
+void dicey_hashset_delete(struct dicey_hashset *const table) {
+    dicey_hashtable_delete((struct dicey_hashtable *) table, NULL);
+}
+
+struct dicey_hashset_iter dicey_hashset_iter_start(struct dicey_hashset *const table) {
+    return (struct dicey_hashset_iter) {
+        ._inner = dicey_hashtable_iter_start((struct dicey_hashtable *) table),
+    };
+}
+
+bool dicey_hashset_iter_next(struct dicey_hashset_iter *const iter, const char **const key) {
+    assert(iter && key);
+
+    return dicey_hashtable_iter_next(&iter->_inner, key, NULL);
+}
+
+bool dicey_hashset_contains(struct dicey_hashset *const table, const char *const key) {
+    return dicey_hashtable_contains((struct dicey_hashtable *) table, key);
+}
+
+bool dicey_hashset_remove(struct dicey_hashset *const table, const char *const key) {
+    return dicey_hashtable_remove((struct dicey_hashtable *) table, key);
+}
+
+enum dicey_hash_set_result dicey_hashset_set(struct dicey_hashset **const set, const char *const key) {
+    assert(set);
+
+    struct dicey_hashtable *map = (struct dicey_hashtable *) *set;
+
+    const enum dicey_hash_set_result res = dicey_hashtable_set(&map, key, &phony, &(void *) { NULL });
+
+    *set = (struct dicey_hashset *) map;
+
+    return res;
+}
+
+uint32_t dicey_hashset_size(struct dicey_hashset *const table) {
+    return dicey_hashtable_size((struct dicey_hashtable *) table);
+}
