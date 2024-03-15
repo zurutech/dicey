1540b5a283702cf206ca107a6c6ea17217f12968
Marco Cilloni <marco.cilloni@zuru.com>
2024-03-15 17:27:23 +0100

    ipc/address: add dup function

--- src/ipc/address.c
+++ src/ipc/address.c
@@ -1,6 +1,5 @@
 // Copyright (c) 2014-2024 Zuru Tech HK Limited, All rights reserved.
 
-#define _CRT_NONSTDC_NO_DEPRECATE 1
 #include <assert.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -14,10 +13,21 @@
 #define HAS_ABSTRACT_SOCKETS 0
 #endif
 
-static const char *connstr_fixup(const char *const connstr) {
+char *cnkdup(const char *const str, const size_t len) {
+    assert(str);
+
+    char *const copy = malloc(len);
+    if (!copy) {
+        return NULL;
+    }
+
+    return memcpy(copy, str, len);
+}
+
+static const char *connstr_fixup(const char *const connstr, const size_t len) {
     assert(connstr);
 
-    char *const str_copy = strdup(connstr);
+    char *const str_copy = cnkdup(connstr, len + 1);
     if (!str_copy) {
         return NULL;
     }
@@ -39,11 +49,33 @@ void dicey_addr_deinit(struct dicey_addr *const addr) {
     }
 }
 
+DICEY_EXPORT enum dicey_error dicey_addr_dup(struct dicey_addr *const dest, const struct dicey_addr src) {
+    assert(dest);
+
+    if (!src.addr || !src.len) {
+        *dest = (struct dicey_addr) { 0 };
+
+        return DICEY_OK;
+    }
+
+    char *const addr_copy = cnkdup(src.addr, src.len + 1);
+    if (!addr_copy) {
+        return DICEY_ENOMEM;
+    }
+
+    *dest = (struct dicey_addr) {
+        .addr = addr_copy,
+        .len = src.len,
+    };
+
+    return DICEY_OK;
+}
+
 const char *dicey_addr_from_str(struct dicey_addr *const dest, const char *const str) {
     assert(dest && str);
 
     const size_t len = strlen(str);
-    const char *const connstr = connstr_fixup(str);
+    const char *const connstr = connstr_fixup(str, len);
     if (!connstr) {
         return NULL;
     }
