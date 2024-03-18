81f99d07809b98de4d31102adbdc8ddf16ee35b7
Marco Cilloni <marco.cilloni@zuru.com>
2024-03-18 16:49:48 +0100

    python: support static linking on Windows

--- bindings/python/dicey/__init__.py
+++ bindings/python/dicey/__init__.py
@@ -4,9 +4,12 @@ if os.name == 'nt':
     fdir = os.path.dirname(os.path.realpath(__file__))
     ldir = os.path.join(fdir, os.pardir, 'dicey.deps', 'bin')
 
-    os.add_dll_directory(ldir)
+    # if the directory exists, add it to the DLL search path
+    # if not, assume that this is a static build. I wish all my users for the best of things
+    if os.path.isdir(ldir):
+        os.add_dll_directory(ldir)
 
-    os.environ['PATH'] += ';' + ldir    
+        os.environ['PATH'] += ';' + ldir    
 
 from .core import *
 from .ipc  import *
