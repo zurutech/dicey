6d67768f56e5efea17542a63bd701a60e68b340e
Marco Cilloni <marco.cilloni@zuru.com>
2024-03-18 10:29:44 +0100

    python/setup: do not set runtime_library_dirs on Windows

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -15,7 +15,7 @@ setup(
             library_dirs=["dicey.deps/lib"],
 
             # set an absolute rpath - this is a temporary hack 
-            runtime_library_dirs = [os.path.join(fdir, 'dicey.deps', 'bin' if os.name == 'nt' else 'lib')]
+            runtime_library_dirs = None if os.name == 'nt' else [os.path.join(fdir, 'dicey.deps', 'lib')]
         )],
         gdb_debug=True,
         compiler_directives={'language_level' : "3"},
