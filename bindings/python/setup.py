4b0d7bc6786ca16b700ffaeb5cf2b30d4e120f67
Marco Cilloni <marco.cilloni@zuru.com>
2024-03-18 09:52:29 +0100

    python/setup: add a few nice little hacks to setup.py

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,6 +1,10 @@
+import os
+
 from setuptools import Extension, setup
 
 from Cython.Build import cythonize
+
+fdir = os.path.dirname(os.path.realpath(__file__))
 
 setup(
     ext_modules = cythonize(
@@ -8,10 +12,13 @@ setup(
                 "dicey/*/*.pyx",
             ],
             libraries=["dicey"],
-            library_dirs=["."],
+            library_dirs=["dicey.deps/lib"],
+
+            # set an absolute rpath - this is a temporary hack 
+            runtime_library_dirs = [os.path.join(fdir, 'dicey.deps', 'bin' if os.name == 'nt' else 'lib')]
         )],
         gdb_debug=True,
         compiler_directives={'language_level' : "3"},
     ),
-    include_dirs=["include"]
+    include_dirs=["dicey.deps/include"],
 )
