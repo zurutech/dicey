665cf917ef8728a73728b459e16ee84182eceb0c
Marco Cilloni <marco.cilloni@zuru.com>
2024-03-18 17:07:36 +0100

    python/setup: fix static build

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -8,15 +8,28 @@ from Cython.Build import cythonize
 fdir = os.path.dirname(os.path.realpath(__file__))
 depsdir = os.path.join(fdir, 'dicey.deps')
 
+bindir = os.path.join(depsdir, 'bin')
 incdir = os.path.join(depsdir, 'include')
 libdir = os.path.join(depsdir, 'lib')
+
+if os.name == 'nt':
+    dicey_dll = os.path.join(bindir, 'dicey.dll')
+    
+    if os.path.isfile(dicey_dll):
+        # assume shared
+        libraries = ['dicey']
+    else:
+        # assume static
+        libraries = ['dicey', 'libuv', 'ws2_32']
+else:
+    libraries = ['dicey', 'uv']
 
 setup(
     ext_modules = cythonize(
         [Extension("*", [
                 "dicey/*/*.pyx",
             ],
-            libraries=["dicey", "uv"],
+            libraries=libraries,
             library_dirs=[libdir],
 
             # set an absolute rpath - this is a temporary hack 
