db9e9eac1795e153b7dc1853a00c15f9f3071e18
Marco Cilloni <marco.cilloni@zuru.com>
2024-03-19 11:28:55 +0100

    python: fix wheel build

--- bindings/python/dicey/__init__.py
+++ bindings/python/dicey/__init__.py
@@ -2,7 +2,7 @@ import os
 
 if os.name == 'nt':
     _fdir = os.path.dirname(os.path.realpath(__file__))
-    _ldir = os.path.join(_fdir, os.pardir, 'dicey.deps', 'bin')
+    _ldir = os.path.join(_fdir, 'deps', 'bin')
 
     # if the directory exists, add it to the DLL search path
     # if not, assume that this is a static build. I wish all my users for the best of things
--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,21 +1,22 @@
 import os
+from pathlib import Path
 import sys
 
-from setuptools import Extension, setup, find_packages
+from setuptools import Extension, setup, find_namespace_packages
 
 from Cython.Build import cythonize
 
-fdir = os.path.dirname(os.path.realpath(__file__))
-depsdir = os.path.join(fdir, 'dicey.deps')
+fdir = Path(__file__).resolve().parent
+depsdir = fdir.joinpath('dicey', 'deps')
 
-bindir = os.path.join(depsdir, 'bin')
-incdir = os.path.join(depsdir, 'include')
-libdir = os.path.join(depsdir, 'lib')
+bindir = depsdir.joinpath('bin')
+incdir = depsdir.joinpath('include')
+libdir = depsdir.joinpath('lib')
 
 if os.name == 'nt':
-    dicey_dll = os.path.join(bindir, 'dicey.dll')
+    dicey_dll = bindir.joinpath('dicey.dll')
     
-    if os.path.isfile(dicey_dll):
+    if dicey_dll.is_file():
         # assume shared
         libraries = ['dicey']
     else:
@@ -24,16 +25,18 @@ if os.name == 'nt':
 else:
     libraries = ['dicey', 'uv']
 
+libdirs = [str(libdir)]
+
 setup(
     ext_modules = cythonize(
         [Extension("*", [
                 "dicey/*/*.pyx",
             ],
             libraries=libraries,
-            library_dirs=[libdir],
+            library_dirs=libdirs,
 
             # set an absolute rpath - this is a temporary hack 
-            runtime_library_dirs = [libdir] if os.name == 'posix' else None,
+            runtime_library_dirs = libdirs if os.name == 'posix' else None,
         )],
         gdb_debug=True,
         compiler_directives={
@@ -41,6 +44,11 @@ setup(
             'language_level' : "3"
         },
     ),
-    packages=find_packages(),
+    packages=find_namespace_packages(),
     include_dirs=[incdir],
+
+    include_package_data=True,
+    package_data={
+        'dicey': ['deps/bin/*.dll', 'deps/lib/*.dylib', 'deps/lib/*.so']
+    },
 )
