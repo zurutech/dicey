798e38d66c778cdc71c11285c227dd7b8a31bed3
Marco Cilloni <marco.cilloni@zuru.com>
2024-04-12 09:56:00 +0200

    typedescr: add support for ->

--- src/ipc/typedescr.c
+++ src/ipc/typedescr.c
@@ -1,11 +1,14 @@
 // Copyright (c) 2014-2024 Zuru Tech HK Limited, All rights reserved.
 
 #include <assert.h>
+#include <ctype.h>
 #include <stdbool.h>
 #include <string.h>
 
-#include <dicey/core/errors.h>
-#include <dicey/core/packet.h>
+#include <dicey/core/type.h>
+#include <dicey/core/views.h>
+
+#include "sup/view-ops.h"
 
 #include "typedescr.h"
 
@@ -17,6 +20,28 @@ static bool is_valid_type(const int ch) {
     return dicey_type_is_valid(ch) || ch == DICEY_VARIANT_ID;
 }
 
+static bool match_exact(const char **const cur, const char *const end, const char *const tag) {
+    assert(cur && *cur && end && tag);
+
+    for (const char *tag_cur = tag; *tag_cur; ++tag_cur) {
+        if (*cur == end || **cur != *tag_cur) {
+            return false;
+        }
+
+        ++*cur;
+    }
+
+    return true;
+}
+
+static void skip_whitespace(const char **const cur, const char *const end) {
+    assert(cur && *cur && end);
+
+    while (*cur != end && isspace(**cur)) {
+        ++*cur;
+    }
+}
+
 static int take_one(const char **const cur, const char *const end) {
     assert(cur && *cur && end);
 
@@ -25,6 +50,20 @@ static int take_one(const char **const cur, const char *const end) {
     }
 
     return *(*cur)++;
+}
+
+static bool parse_arrow(const char **const cur, const char *const end) {
+    assert(cur && *cur && end);
+
+    skip_whitespace(cur, end);
+
+    if (!match_exact(cur, end, "->")) {
+        return false;
+    }
+
+    skip_whitespace(cur, end);
+
+    return true;
 }
 
 static bool parse_type(const char **begin, const char *end);
@@ -95,10 +134,54 @@ static bool parse_type(const char **const cur, const char *const end) {
     }
 }
 
-bool dicey_typedescr_is_valid(const char *typedescr) {
-    assert(typedescr);
+bool dicey_typedescr_is_valid(const char *const typedescr) {
+    return dicey_typedescr_parse(typedescr, &(struct dicey_typedescr) { 0 });
+}
 
+bool dicey_typedescr_parse(const char *typedescr, struct dicey_typedescr *const descr) {
+    assert(typedescr && descr);
+
+    const char *const root = typedescr;
     const char *const end = typedescr + strlen(typedescr);
 
-    return parse_type(&typedescr, end) && typedescr == end;
+    if (!parse_type(&typedescr, end)) {
+        return false;
+    }
+
+    if (typedescr == end) {
+        *descr = (struct dicey_typedescr) {
+            .kind = DICEY_TYPEDESCR_VALUE,
+            .value = typedescr,
+        };
+
+        return true;
+    }
+
+    const struct dicey_view input = dicey_view_from(root, typedescr - root);
+
+    if (!parse_arrow(&typedescr, end)) {
+        return false;
+    }
+
+    if (typedescr == end) {
+        return false;
+    }
+
+    const char *const output_start = typedescr;
+
+    if (!parse_type(&typedescr, end) || typedescr != end) {
+        return false;
+    }
+
+    const struct dicey_view output = dicey_view_from(output_start, end - output_start);
+
+    *descr = (struct dicey_typedescr){
+        .kind = DICEY_TYPEDESCR_FUNCTIONAL,
+        .op = {
+            .input = input,
+            .output = output,
+        },
+    };
+
+    return true;
 }
--- src/ipc/typedescr.h
+++ src/ipc/typedescr.h
@@ -5,8 +5,30 @@
 
 #include <stdbool.h>
 
-#include <dicey/core/errors.h>
+#include <dicey/core/views.h>
+
+enum dicey_typedescr_kind {
+    DICEY_TYPEDESCR_INVALID,
+
+    DICEY_TYPEDESCR_VALUE,
+    DICEY_TYPEDESCR_FUNCTIONAL,
+};
+
+struct dicey_typedescr_op {
+    struct dicey_view input;
+    struct dicey_view output;
+};
+
+struct dicey_typedescr {
+    enum dicey_typedescr_kind kind;
+
+    union {
+        const char *value;
+        struct dicey_typedescr_op op;
+    };
+};
 
 bool dicey_typedescr_is_valid(const char *typedescr);
+bool dicey_typedescr_parse(const char *typedescr, struct dicey_typedescr *descr);
 
 #endif // WQWGWWXACS_TYPEDESCR_H
