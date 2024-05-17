ea2fb3f932ff52b9c4f8626606d6e62eac8e960c
Alessio Paoletti <alessio.paoletti@zuru.com>
2024-05-17 11:14:23 +0200

    Fixed clang-format

--- src/wirefmt/value-validate.c
+++ src/wirefmt/value-validate.c
@@ -81,7 +81,8 @@ static bool checksig(struct dicey_view *const sig, const struct dicey_value *con
 
             const int cpar = skip_char(sig);
             assert(cpar == ']'); // TODO: export this as a constant, this requires a new header though
-            (void) cpar;         // MSVC discards the asserts before parsing them, so cpar appears unused: (void) is the historic way to do [[maybe_unused]]
+            (void) cpar; // MSVC discards the assert before parsing it, so cpar appears unused: (void) is the historic
+                         // way to do [[maybe_unused]]
 
             return true;
         }
