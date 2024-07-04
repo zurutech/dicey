809052f3583143d01f5c243e953a5b29b6691ad1
Marco Cilloni <marco.cilloni@zuru.com>
2024-07-04 11:33:03 +0200

    version: bump

--- include/dicey/core/version.h
+++ include/dicey/core/version.h
@@ -59,10 +59,10 @@ DICEY_EXPORT int dicey_version_cmp(struct dicey_version a, struct dicey_version 
  */
 #define DICEY_LIB_VERSION_MAJOR 0
 #define DICEY_LIB_VERSION_MINOR 3
-#define DICEY_LIB_VERSION_PATCH 0
+#define DICEY_LIB_VERSION_PATCH 1
 #define DICEY_LIB_VERSION_STRING #DICEY_LIB_VERSION_MAJOR "." #DICEY_LIB_VERSION_MINOR "." #DICEY_LIB_VERSION_PATCH
 
-#define DICEY_LIB_VER_INT 0x00000300
+#define DICEY_LIB_VER_INT 0x00000301
 
 #if defined(__cplusplus)
 }
